c62adf2528d19226e3afbbc4d34ce1afe3662ca3
f0rt3hlulz <lulzm4k3r@gmail.com>
2022-09-24 03:51:25 +0700

    after 2nd review

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -29,12 +29,12 @@ class RecipeAdmin(admin.ModelAdmin):
         'name', 'author__username', 'tags__name', 'ingredients__name'
     )
 
-    def ingrs(self, obj):
+    def get_ingredients(self, obj):
         return ', '.join(list([
             list(i.values())[0] for i in obj.ingredients.values('name')
         ]))
 
-    def tgs(self, obj):
+    def get_tags(self, obj):
         return ', '.join(list([
             list(i.values())[0] for i in obj.tags.values('name')
         ]))
