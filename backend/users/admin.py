6ca6e954c15f8f5f670935123abae30042615045
Ярцев Илья Александрович <lulzm4k3r@gmail.com>
2022-09-26 15:24:11 +0700

    after 3rd review

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib import admin
+from django.contrib.auth.forms import (ReadOnlyPasswordHashField,
+                                       UserCreationForm)
 
 from .models import User
 
@@ -11,10 +13,18 @@ class UserAdminForm(forms.ModelForm):
         
         super(UserAdminForm, self).__init__(*args, **kwargs)
         self.fields['password'].widget = forms.PasswordInput()
-
+    
     class Meta:
         model = User
         fields = '__all__'
+
+class CustomUserCreationForm(UserCreationForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = UserCreationForm.Meta.model
+        fields = ('__all__')
+        field_classes = UserCreationForm.Meta.field_classes
 
 
 @admin.register(User)
@@ -24,9 +34,18 @@ class UserAdmin(admin.ModelAdmin):
         'is_superuser', 'is_staff', 'date_joined', 'followers_count',
     )
     empty_value_display = EMPTY
-    form = UserAdminForm
+    form = CustomUserCreationForm
     search_fields = ('email', 'username')
     readonly_fields = ('date_joined', 'last_login')
 
     def followers_count(self, obj):
         return obj.followers.count()
+
+    def save_model(self, request, obj, form, change):
+        if obj.pk:
+            if 'pbkdf2_sha256$' not in obj.password:
+                obj.set_password(obj.password)
+            obj.is_staff = True if obj.role == 'admin' else False
+            obj.is_superuser = True if obj.role == 'admin' else False
+        obj.save()
+   
