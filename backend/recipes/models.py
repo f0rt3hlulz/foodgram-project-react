6996c5741ab1acdc74d9cfdadfeaffdf9ddea014
Ярцев Илья Александрович <lulzm4k3r@gmail.com>
2022-09-26 12:24:22 +0700

    after 3rd review

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -12,8 +12,8 @@ class Ingredient(models.Model):
     measurement_unit = models.CharField(max_length=20)
 
     class Meta:
-        verbose_name = 'Ингредиент',
-        verbose_name_plural = 'Ингредиенты',
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
         ordering = ('name',)
 
     def __str__(self):
