c45f25c9b742201acfc13576874a06e47ee6a422
Ярцев Илья Александрович <lulzm4k3r@gmail.com>
2022-09-26 14:04:25 +0700

    after 3rd review

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -23,10 +23,7 @@ class UserAdmin(admin.ModelAdmin):
         'username', 'email', 'first_name', 'last_name',
         'is_superuser', 'is_staff', 'date_joined', 'followers_count',
     )
-    list_editable = (
-        'username',
-        'password'
-    )
+    readonly_fields = ('date_started',)
     empty_value_display = EMPTY
     form = UserAdminForm
     search_fields = ('email', 'username')
