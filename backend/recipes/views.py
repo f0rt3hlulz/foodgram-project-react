26cf8a991b5851ba5b024c7f7c3833955243a007
Ярцев Илья Александрович <lulzm4k3r@gmail.com>
2022-09-26 17:01:08 +0700

    after 3rd review

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import get_object_or_404
 from django.http.response import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from users.serializers import RecipeForUserSerializer
 
 from .filters import IngredientFilter, RecipeFilter
